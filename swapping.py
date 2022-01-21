b7733b622abd86807f3d434312ad6dd1acd3515d
RajasMahajan <73475718+RajasMahajan@users.noreply.github.com>
2022-01-21 14:52:58 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,21 @@
+
+exa = open("one.txt", "a+")
+
+
+exa2 = open("two.txt", "a+")
+
+
+exa.seek(0)
+exa2.seek(0)
+ee = exa2
+exa.write('ee')
+exa2.write('exa1')
+read2 = ee.readline()
+read1 = exa2.readline()
+print(read1)
+print(read2)
+
+#hejkwhdiajkhailkjioklsadajskdmhsqdoiuadaskldajdzklcidsada,sdasmd if oerou can to dor 
+# qwasdlasda;sdjasidoqrywuidjkasl
+# 
+# 
